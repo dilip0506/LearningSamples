79b5e14e7a0b3f9e670b2db1971bd95cfd065cbf
Dilip Nasu <ndilip0506@gmail.com>
2024-04-29 11:10:35 -0500

    Graph

--- CodeSamples/AlgorithmsAndDataStructures/DataStructures/DataStuctures/Graphs/Graph.cs
+++
@@ -1,8 +0,0 @@
-namespace DataStuctures.Graphs
-{
-    public class Graph
-    {
-        public int Vertex { get; set; }
-        public HashSet<int> Edges { get; set; } = new HashSet<int>();
-    }
-}
