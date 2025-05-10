6d113947f37071b323a713bb9fed056b808ce72a
Dilip N <dilip@no-reply.com>
2025-05-09 19:37:31 -0500

    practise updates

---
+++ CodeSamples/MirosoftTech/DesignPatterns/WebBlog/WebBlog.Delegate/TestRunner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBlog.Delegate
+{
+    public static class TestRunner
+    {
+        public static void Run()
+        {
+            var delegateHandler = new DelegateHandler();
+            delegateHandler.PrintMessage();
+        }
+    }
+}
